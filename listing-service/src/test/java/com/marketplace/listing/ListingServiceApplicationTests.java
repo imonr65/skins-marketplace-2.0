1fa7c16e2c5486901e0d693822c86ab2211fbd8c
imonr65 <radmirshar65@gmail.com>
2026-08-19 19:51:11 +0500

    Первые шаги
    Добавил папки под сервисы, общий pom и вынес информацию про Event Storming из Readme.md в другой файл

---
+++ listing-service/src/test/java/com/marketplace/listing/ListingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplace.listing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
